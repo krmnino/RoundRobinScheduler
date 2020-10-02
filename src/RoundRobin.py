a47936657cc4b4a403b9514ddd6a236ccd7dfe31
krmnino <kurt.manrique.n@gmail.com>
2020-10-02 12:21:13 -0400

    added user input to run the program

--- src/RoundRobin.py
+++ src/RoundRobin.py
@@ -1,5 +1,13 @@
+import sys
+import os
 from queue import Queue
 from Jobs import Jobs
+
+def get_parameters(raw_parameters):
+    time_slice = int(raw_parameters[1])
+    jobs_list = raw_parameters[2].split(',')
+    jobs_list = [int(i) for i in jobs_list]
+    return [time_slice, jobs_list]
 
 def print_header(jobs):
     print('%5s'%('Time'), end='')
@@ -18,7 +26,7 @@ def build_job_queue(jobs_list):
     return jobs
 
 def round_robin(time_slice, jobs_queue):
-    jobs_initial_state = [i for i in jobs_queue]
+    jobs_record = [i for i in jobs_queue]
     runtime = 0
     runtime_time_slice = 0
     job_finished = False
@@ -27,7 +35,7 @@ def round_robin(time_slice, jobs_queue):
             break
         if jobs_queue[0].time_requested <= 0:
             finished = jobs_queue.pop(0)
-            for i in jobs_initial_state:
+            for i in jobs_record:
                 if i.tag == finished.tag:
                     i.finished = True
             job_finished = True
@@ -41,7 +49,7 @@ def round_robin(time_slice, jobs_queue):
         jobs_queue[0].time_requested -= 1
         job_finished = False
         print('%5s'%(runtime), end='')
-        for i in jobs_initial_state:
+        for i in jobs_record:
             if i.tag == current_job.tag:
                 print('%5s'%('X'), end='')
             elif i.finished:
@@ -52,10 +60,14 @@ def round_robin(time_slice, jobs_queue):
         runtime += 1
         runtime_time_slice += 1
 
-#jobs listed in order of arrival
-jobs_list = [20, 30, 20]
-time_slice = 30
-
-job_queue = build_job_queue(jobs_list)
+#index      data
+#    0      time slice
+#    1      jobs          
+if len(sys.argv) != 3:
+    print('usage: python RoundRobin.py <TimeSlice> <Job1, Job2, ...>')
+    print('Example: python RoundRobin.py 2 20,30,20')
+    sys.exit(1)
+user_input = get_parameters(list(sys.argv))
+job_queue = build_job_queue(user_input[1])
 print_header(job_queue)
-round_robin(time_slice, job_queue)
+round_robin(user_input[0], job_queue)
